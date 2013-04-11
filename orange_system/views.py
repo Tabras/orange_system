0429fa7d7e04e5c0f7e22239ac14291f26b17423
AllenPearson <allentheprogrammer@gmail.com>
2013-04-10 22:28:27 -0500

    completed pages and links

--- orange_system/views.py
+++ orange_system/views.py
@@ -11,7 +11,7 @@ from .models import (
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
-    return {'project': 'capstone_project'}
+    return {'project': 'orange_system'}
 
 @view_config(route_name='search', renderer='templates/searchTemplate.pt')
 def search_view(request):
@@ -23,6 +23,31 @@ def search_view(request):
         for row in q:
             print row.firstName
     return {'project': 'orange_system'}
+    
+@view_config(route_name='customer', renderer='templates/customerTemplate.pt')
+def customer_view(request):
+    return {'project': 'orange_system'}
+    
+@view_config(route_name='order', renderer='templates/orderTemplate.pt')
+def order_view(request):
+    return {'project': 'orange_system'}
+    
+@view_config(route_name='service', renderer='templates/serviceTemplate.pt')
+def service_view(request):
+    return {'project': 'orange_system'}
+    
+@view_config(route_name='part', renderer='templates/partTemplate.pt')
+def part_view(request):
+    return {'project': 'orange_system'}
+    
+@view_config(route_name='report', renderer='templates/reportTemplate.pt')
+def report_view(request):
+    return {'project': 'orange_system'}
+    
+@view_config(route_name='todo', renderer='templates/todoTemplate.pt')
+def todo_view(request):
+    return {'project': 'orange_system'}
+    
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
